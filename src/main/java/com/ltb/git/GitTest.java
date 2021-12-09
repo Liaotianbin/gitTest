0293082607fc114334cd67eaa8ed2201f9063d3e
Liaotianbin <1640658956@qq.com>
2021-12-09 10:51:23 +0800

    second commit

--- src/main/java/com/ltb/git/GitTest.java
+++ src/main/java/com/ltb/git/GitTest.java
@@ -3,5 +3,6 @@ package com.ltb.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!22222");
     }
 }
